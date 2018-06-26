25fc91a466959cd5b3f411c37c5216f4131f6afb
VladDyachenko <vladdyachenko487@gmail.com>
2018-06-26 13:42:40 +0300

    first commit

--- src/com/gmail/vlad/dyachenko487/Main.java
+++ src/com/gmail/vlad/dyachenko487/Main.java
@@ -1,9 +1,20 @@
 package com.gmail.vlad.dyachenko487;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("введите любое 5-и значное число + Press Enter");
+        int number = sc.nextInt();
+
+        System.out.println(number / 10000);
+        System.out.println(number % 10000 / 1000);
+        System.out.println(number % 1000 / 100);
+        System.out.println(number % 100 / 10);
+        System.out.println(number % 10);
+        sc.close();
 
     }
 }
